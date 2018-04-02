52071fbdbb4a3133ec5578f310162b5989fd8964
nshakirova <nailya.shakirova@rtlabs.ru>
2018-04-02 14:37:05 +0300

    HomeWork Task4

---
+++ HomeWorks/Task 4/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int parse(char input[]) {
+        int strLen = input.length;
+        int res = 0;
+        for (int i = 0; i <= strLen - 1; i++){
+            res = res + (((int)(input[i] - '0')) * (int)Math.pow(10,(strLen - 1 - i)));
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите строку цифр:");
+
+        char str[] = scanner.nextLine().toCharArray();
+        int Res = parse(str);
+        
+        System.out.println(Res);
+    }
+}
