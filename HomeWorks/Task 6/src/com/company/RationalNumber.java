1fc22fa77d9d6607c1864ad02bcf5d8b4a5044dc
nshakirova <nailya.shakirova@rtlabs.ru>
2018-04-06 11:28:15 +0300

    HomeWork Task6

---
+++ HomeWorks/Task 6/src/com/company/RationalNumber.java
@@ -0,0 +1,62 @@
+package com.company;
+
+/**
+ * Created by nailya.shakirova on 06.04.2018.
+ */
+public class RationalNumber {
+    private int dividend;
+    private int divider;
+
+    public RationalNumber() {
+        this.dividend = 0;
+        this.divider = 1;
+    }
+
+    public RationalNumber (int dividend, int divider) {
+        this.dividend = dividend;
+        if (divider == 0) {
+           this.divider = 1;
+        }
+        else {
+            this.divider = divider;
+        }
+    }
+
+    public int getDivident() {
+        return dividend;
+    }
+
+    public int getDivider() {
+        return divider;
+    }
+
+    void show () {
+        System.out.println(dividend + "/" + divider);
+    }
+
+    double toDouble() {
+        double x = (double)(dividend/divider);
+        return x;
+    }
+
+    int nod (int m, int n) {
+        while (m != 0 && n != 0) {
+            if (m > n) {
+                m = m % n;
+            }
+            else {
+                n = n % m;
+            }
+        }
+        return m + n;
+    }
+
+    void optimize(){
+        int d = nod(dividend,divider);
+//        RationalNumber optimRationalNumber;
+        RationalNumber optimRationalNumber = new RationalNumber (this.dividend / d, this.divider / d);
+//        this.dividend = this.dividend / d;
+//        this.divider = this.divider / d;
+        System.out.println(optimRationalNumber.toDouble());
+    }
+}
